d9448b4947ff69bcf3c56a5afef211ae9a130a2c
Megan <megan.miller@smoldev.dev>
2020-02-10 15:57:26 -0600

    Adding comments to prime solution

--- src/prime.py
+++ src/prime.py
@@ -3,6 +3,11 @@
 x = input("Enter a number: ")
 
 # Actual Final Solution.
+# Changed implementation to be a function as suggested by TL,
+# also changed the for loop implementation to use not rather than 
+# to have multiple if/else. This allows me not to have to break the 
+# loop to get all correct primes.
+
 
 def prime(n):
     for m in range(2, int(n**0.5)+1):
