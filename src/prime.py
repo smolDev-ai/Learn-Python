3c4121640802ade45249145268d21db8024f0673
Megan <megan.miller@smoldev.dev>
2020-02-10 16:00:22 -0600

    changing the variable names to be more readable should I need to reference this code again.

--- src/prime.py
+++ src/prime.py
@@ -4,14 +4,14 @@ x = input("Enter a number: ")
 
 # Actual Final Solution.
 # Changed implementation to be a function as suggested by TL,
-# also changed the for loop implementation to use not rather than 
-# to have multiple if/else. This allows me not to have to break the 
+# also changed the for loop implementation to use not rather than
+# to have multiple if/else. This allows me not to have to break the
 # loop to get all correct primes.
 
 
-def prime(n):
-    for m in range(2, int(n**0.5)+1):
-        if not n % m:
+def prime(num):
+    for iterator in range(2, int(num**0.5)+1):
+        if not num % iterator:
             return "Not Prime!"
     return "Prime!"
 
