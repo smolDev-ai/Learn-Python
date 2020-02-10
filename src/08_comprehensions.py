d44684981628a4df518551ee823f648356412f3d
Megan <megan.miller@smoldev.dev>
2020-02-09 19:20:17 -0600

    fixing linter issues

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -4,7 +4,8 @@ They essentially act as a terse and concise way of initializing
 and populating a list given some expression that specifies how
 the list should be populated.
 
-Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
+Take a look at
+https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
 for more info regarding list comprehensions.
 """
 
