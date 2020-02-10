1f45c203b58c18492af18f4a939aa4174fda4e6b
Megan <megan.miller@smoldev.dev>
2020-02-09 20:59:37 -0600

    Calendar

--- src/14_cal.py
+++ src/14_cal.py
@@ -18,15 +18,41 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
-prompted input. Also, the brackets around year are to denote that the argument is
-optional, as this is a common convention in documentation.
+Note: the user should provide argument input (in the initial call to run the
+file) and not
+prompted input. Also, the brackets around year are to denote that the argument
+is optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
-it should use today’s date to get the month and year.
+This would mean that from the command line you would call:
+`python3 14_cal.py 4 2015`
+to print out a calendar for April in 2015, but if you omit either the year or
+both values, it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+"""
+REMINDER: sys.argsv is how you access the arguments as the file is called.
+
+"""
+
+# PSEUDO CODE:
+# if sys.argsv is only one argument (the file)
+# return now.
+# otherwise return sys.argsv specified time.
+
+arg = sys.argv
+
+if len(arg) == 1:
+    # user didn't specify a month or year.
+    month = datetime.today().month
+    year = datetime.today().year
+elif len(arg) == 3:
+    # user specified a month and a year
+    month = int(sys.argv[1])
+    year = int(sys.argv[2])
+
+
+calendar.TextCalendar().prmonth(year, month)
