696f1cf1ef973103f26c83ab75cfda859baf6b9d
Megan <megan.miller@smoldev.dev>
2020-02-10 14:23:35 -0600

    Prime Stretch failing on certain numbers

--- src/prime.py
+++ src/prime.py
@@ -1,12 +1,39 @@
 # Current BigO == O(n)
 
 x = input("Enter a number: ")
+"""
+# Current Solution
+# Issues:
+# 21 is returned as Prime.
+
+if int(x) == 2:
+    print("Prime")
+
+if int(x) > 1:
+    for i in range(2, int(x)//2):
+        if (int(x) % i) == 0:
+            print("Not Prime!")
+            break
+        else:
+            print("Prime!")
+            break
+else:
+    print("Prime!")
+"""
+
+# Initial solution
+# Issues:
+# 21 is returned as Prime
+
+if int(x) <= 1:
+    print("Prime")
+
 
 if (int(x) % int(x) == 0 and int(x) % 1 == 0):
-    if (int(x) % 2 == 0 or int(x) % 5 == 0):
-        if(int(x) == 2 or int(x) == 5):
-            print("Prime!")
+    for i in range(2, int(x)//2):
+        if(int(x) % i == 0):
+            print("Not Prime")
         else:
-            print("Not Prime!")
+            print("Prime")
     else:
         print("Prime!")
